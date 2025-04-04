6105c786d8aa4eb7338b50a73eee60c03c147ced
evanmm3 <evanmm3@illinois.edu>
2025-04-04 16:01:14 -0500

    more testing

--- acoustic.py
+++ acoustic.py
@@ -5,6 +5,7 @@ necessary for simulating sound in a given environment.
 
 import pyroomacoustics as pra
 import numpy as np
+import pymesh
 
 class Acoustic(pra.room.Room):
 
@@ -15,11 +16,39 @@ class Acoustic(pra.room.Room):
         pass
 
 
-    def build_obj():
+    def build_obj(filename: str):
         """
         Build the room from an OBJ file.
         """
-        pass
+        mesh = pymesh.load_mesh(filename)
+        material = pra.Material(energy_absorption=0.2, scattering=0.1)
+        ntriang, nvec, npts = mesh
+        size_reduc_factor = 100  # to get a realistic room size (not 3km)
+
+        # create one wall per triangle
+        walls = []
+        for w in range(ntriang):
+            walls.append(
+                pra.wall_factory(
+                    mesh.faces[w].T / size_reduc_factor,
+                    material.energy_absorption["coeffs"],
+                    material.scattering["coeffs"],
+            )
+        )
+
+        room = (
+            pra.Room(
+                walls,
+                fs=16000,
+                max_order=3,
+                ray_tracing=True,
+                air_absorption=True,
+            )
+            .add_source([0.0, 0.0, 0.0])   
+        )
+
+        room.plot(img_order=1)
+        plt.show()
 
 
     def __init__(self, filename: str):
@@ -27,8 +56,7 @@ class Acoustic(pra.room.Room):
         self.sample_rate = 44100
         self.speed_of_sound = 343.0
 
-        w = pra.wall.Wall()
-        return
+        self.build_obj(filename)
     
     def simulate(self):
         return 0
--- room_from_stl.py
+++ room_from_stl.py
@@ -1,14 +1,16 @@
 """
-This sample program demonstrate how to import a model from an STL file.
-Currently, the materials need to be set in the program which is not very practical
-when different walls have different materials.
+Random demo file for room acoustic simulation on an STL file.
 
-The STL file was kindly provided by Diego Di Carlo (@Chutlhu).
+References:
+https://github.com/LCAV/pyroomacoustics/blob/master/notebooks/pyroomacoustics_demo.ipynb
+https://github.com/LCAV/pyroomacoustics/blob/master/examples/room_from_stl.py
 """
 
 import argparse
 import os
 from pathlib import Path
+from scipy.io import wavfile
+from scipy.signal import fftconvolve
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -25,7 +27,7 @@ except ImportError as err:
     )
     raise err
 
-default_stl_path = Path(__file__).parent / "data/INRIA_MUSIS.stl"
+default_stl_path = Path(__file__).parent / "resources/INRIA_MUSIS.stl"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Basic room from STL file example")
@@ -39,7 +41,7 @@ if __name__ == "__main__":
     # with numpy-stl
     the_mesh = mesh.Mesh.from_file(args.file)
     ntriang, nvec, npts = the_mesh.vectors.shape
-    size_reduc_factor = 10.0  # to get a realistic room size (not 3km)
+    size_reduc_factor = 500.0  # to get a realistic room size (not 3km)
 
     # create one wall per triangle
     walls = []
@@ -52,24 +54,39 @@ if __name__ == "__main__":
             )
         )
 
+    fs, signal = wavfile.read('sounds/piano.wav')
+    signal = signal.astype(np.float32)/32768.0 # required.
+
     room = (
         pra.Room(
             walls,
-            fs=16000,
+            fs=fs,
             max_order=3,
             ray_tracing=True,
             air_absorption=True,
         )
-        #.add_source([0.0, 0.0, 0.0])
-        #.add_microphone_array(np.c_[[-6.5, 8.5, 3 + 0.1], [-6.5, 8.1, 3 + 0.1]])
+        .add_source([-2.0, 2.0, 1.8], signal=signal)
+        .add_microphone_array(np.c_[[-6.5, 8.5, 3 + 0.1], [-6.5, 8.1, 3 + 0.1]])
     )
 
     # compute the rir
-    #room.image_source_model()
-    #room.ray_tracing()
-    #room.compute_rir()
-    #room.plot_rir()
+    room.image_source_model()
+    room.ray_tracing()
+    room.compute_rir()
+    room.plot_rir()
+
+    room.simulate()
 
     # show the room
     room.plot(img_order=1)
+    plt.show()
+
+    print(f'Shapes:\n Original:{signal.shape}\n Simulation:{room.mic_array.signals.shape}')
+
+    wavfile.write(filename='sounds/output.wav', rate=fs, data=room.mic_array.signals[0,:])
+
+    plt.plot(np.abs(signal), label='Original')
+    plt.plot(np.abs(room.mic_array.signals[0, :]), label='Simulated')
+    plt.title('Signal comparison')
+    plt.legend()
     plt.show()
